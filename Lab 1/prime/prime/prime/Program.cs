06e11e6dd14262ba881c9ba20d0fb40101c0e443
Zhangir <zhangir.zhandauov@gmail.com>
2018-01-16 16:24:36 +0600

    asd

---
+++ Lab 1/prime/prime/prime/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime
+{
+    class Program
+    {
+        public static bool isprime(int a)
+        {
+            for (int i = 2; i * i <= a; i++)
+            {
+                if (a % i == 0)
+                    return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string[] arr = a.Split(' ');
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (int.Parse(arr[i]) == 2 || int.Parse(arr[i]) == 3)
+                {
+                    Console.WriteLine(int.Parse(arr[i]));
+                    continue;
+                }
+                if (isprime(int.Parse(arr[i])))
+                    Console.WriteLine(arr[i], " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 1/student/student/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    class Student
+    {
+        public string name;
+        public string sname;
+        public int age;
+        public double gpa;
+        public string univ;
+
+        public Student()
+        {
+            name = "Zhangir";
+            sname = "Zhandauov";
+            age = 17;
+            gpa = 4;
+        }
+        public Student(string a, string b, int f, double d)
+        {
+            name = a;
+            sname = b;
+            age = f;
+            gpa = d;
+        }
+
+
+        public override string ToString()
+        {
+            return sname + " " + name + " " + age + " " + gpa;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student b = new Student("Zhanga", "Zhandauov", 17, 1.11);
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+    }
+}
